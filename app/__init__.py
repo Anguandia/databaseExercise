f1d06bdd766d5b963d470969d238cac1907c405c
Anguandia <mikeanguandia@gmail.com>
2019-01-13 23:45:59 +0300

    kjjih

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask_api import FlaskAPI
 from instance.config import app_config
 
 
 def create_app(config_name):
     from . import views
-    app = Flask(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
     app.config.from_object(app_config[config_name])
 
