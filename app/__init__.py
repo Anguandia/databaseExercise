730ca02280dda792132d814d0e1c5564c808bfe9
Anguandia <mikeanguandia@gmail.com>
2019-01-21 00:31:14 +0300

    djh

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from instance.config import app_config
+from config.config import app_config
 from app.db import Db
 
 db_name='twice'
@@ -9,7 +9,7 @@ db_name='twice'
 def create_app(config_name):
     from . import views
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
+    #app.config.from_pyfile('config.py')
     app.config.from_object(app_config[config_name])
     app.db = Db(db_name)
 
